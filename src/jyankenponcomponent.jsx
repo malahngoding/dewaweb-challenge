a091d0f0838baf308fa6d0532d4e94692baa1156
Athhar Kautsar <athharkautsar14@gmail.com>
2021-11-25 21:15:36 +0700

    [UPDATE] Fixing Bug

--- src/jyankenponcomponent.jsx
+++ src/jyankenponcomponent.jsx
@@ -1,83 +1,86 @@
 import React, { useEffect, useState } from "react";
 
 export const JyankenponComponent = () => {
-  const [playerSelection, setPlayerSelection] = useState("");
-  const [enemySelection, setEnemySelection] = useState("");
-  const [winner, setWinner] = useState("");
+	const [playerSelection, setPlayerSelection] = useState("");
+	const [enemySelection, setEnemySelection] = useState("");
+	const [winner, setWinner] = useState("");
 
-  const shuffleEnemyCard = () => {
-    const array = ["Gunting", "Kertas", "Batu"];
-    return array.sort(() => Math.random() - 0.5)[0];
-  };
+	const shuffleEnemyCard = () => {
+		const array = ["Gunting", "Kertas", "Batu"];
+		return array.sort(() => Math.random() - 0.5)[0];
+	};
 
-  useEffect(() => {
-    if (playerSelection !== "") {
-      setEnemySelection(shuffleEnemyCard());
-      // Logic
-      if (playerSelection === "Gunting") {
-        switch (enemySelection) {
-          case "Kertas":
-            setWinner("Enemy Win");
+	useEffect(() => {
+		// Logic
+		if (playerSelection === "Gunting") {
+			switch (enemySelection) {
+				case "Kertas":
+					setWinner("Player Win");
 
-            break;
-          case "Batu":
-            setWinner("Player Win");
+					break;
+				case "Batu":
+					setWinner("Enemy Win");
 
-            break;
-          case "Gunting":
-            setWinner("Draw");
+					break;
+				case "Gunting":
+					setWinner("Draw");
 
-            break;
-        }
-      }
-      if (playerSelection === "Kertas") {
-        switch (enemySelection) {
-          case "Gunting":
-            setWinner("Enemy Win");
+					break;
+			}
+		}
+		if (playerSelection === "Kertas") {
+			switch (enemySelection) {
+				case "Gunting":
+					setWinner("Enemy Win");
 
-            break;
-          case "Batu":
-            setWinner("Player Win");
+					break;
+				case "Batu":
+					setWinner("Player Win");
 
-            break;
-          case "Kertas":
-            setWinner("Draw");
+					break;
+				case "Kertas":
+					setWinner("Draw");
 
-            break;
-        }
-      }
-      if (playerSelection === "Batu") {
-        switch (enemySelection) {
-          case "Kertas":
-            setWinner("Enemy Win");
+					break;
+			}
+		}
+		if (playerSelection === "Batu") {
+			switch (enemySelection) {
+				case "Kertas":
+					setWinner("Enemy Win");
 
-            break;
-          case "Gunting":
-            setWinner("Player Win");
+					break;
+				case "Gunting":
+					setWinner("Player Win");
 
-            break;
-          case "Batu":
-            setWinner("Draw");
+					break;
+				case "Batu":
+					setWinner("Draw");
 
-            break;
-        }
-      }
-    }
-  }, [playerSelection]);
+					break;
+			}
+		}
+	}, [playerSelection, enemySelection]);
 
-  const handleClick = (item) => {
-    setPlayerSelection(item);
-  };
+	useEffect(() => {
+		if (playerSelection !== "") {
+			setEnemySelection(shuffleEnemyCard());
+		}
+	}, [playerSelection]);
 
-  return (
-    <div style={{ margin: "2em" }}>
-      <p>Pemain memilih : {playerSelection}</p>
-      <p>Musuh memilih : {enemySelection}</p>
-      <br />
-      <p>Pemenangnya adalah : {winner}</p>
-      <button onClick={() => handleClick("Gunting")}>Gunting</button>
-      <button onClick={() => handleClick("Kertas")}>Kertas</button>
-      <button onClick={() => handleClick("Batu")}>Batu</button>
-    </div>
-  );
+	const handleClick = (item) => {
+		setPlayerSelection(item);
+	};
+
+	return (
+		<div style={{ margin: "2em" }}>
+			<p>Pemain memilih : {playerSelection}</p>
+			<p>Musuh memilih : {enemySelection}</p>
+			<br />
+			<p>Pemenangnya adalah : {winner}</p>
+			<button onClick={() => handleClick("Gunting")}>Gunting</button>
+			<button onClick={() => handleClick("Kertas")}>Kertas</button>
+			<button onClick={() => handleClick("Batu")}>Batu</button>
+		</div>
+	);
 };
